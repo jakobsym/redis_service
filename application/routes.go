4963de9cb4deb90bebed1dd9b9a8fc41675332c8
jakobsym <jakob.symchych@my.utsa.edu>
2024-02-11 14:43:36 -0600

    clean routes up, making subroute for /orders

--- application/routes.go
+++ application/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jakobsym/redis_service/handler"
 )
 
 func loadRoutes() *chi.Mux {
@@ -16,5 +17,21 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	// subroute for /orders
+	// loadOrderRoutes is function that recieves subroute
+	// which allows all order routes to be placed in loadOrderRoutes
+	// and automatically prefaces routes with '/order'
+	router.Route("/orders", loadOrderRoutes)
 	return router
+}
+
+// routes -----> handlers(figure what to do with request from route)
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
