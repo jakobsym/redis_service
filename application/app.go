7e7a6a5a47c99423eb0047c3ea7107e5f9e0768b
jakobsym <jakob.symchych@my.utsa.edu>
2024-02-16 13:55:06 -0600

    implement graceful shutdowns of db/server

--- application/app.go
+++ application/app.go
@@ -4,30 +4,73 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/go-redis/redis/v8"
 )
 
 // store application dependencies
+
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 // construrctor to init application
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 	return app
 }
 
 // start application at specified route
 func (a *App) Start(ctx context.Context) error {
+
 	server := &http.Server{
 		Addr:    ":3000",
 		Handler: a.router,
 	}
-	err := server.ListenAndServe()
+	// check if db online
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to connect redis: %w", err)
 	}
-	return nil // success
+
+	// close db using 'defer'
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			err := fmt.Errorf("failed to close redis: %w", err)
+			fmt.Println(err)
+		}
+	}()
+
+	fmt.Println("Starting server.")
+
+	ch := make(chan error, 1)
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		// give time for anything to finish before shutdown
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		return server.Shutdown(timeout)
+	}
+
+	// can check if channel was closed below
+	// err, open := <-ch
+	/*
+		if !open {
+			// channel is closed
+		}
+	*/
 }
--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
+/*
+-- starting redis using homebrew:
+	-> $ brew services start redis
+	-> $ redis-cli (opens redis)
+	-> $ brew services stop redis
+*/
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/jakobsym/redis_service/application"
 )
@@ -12,7 +20,15 @@ import (
 // can send curl commands to server to test if working as intended
 func main() {
 	app := application.New()
-	err := app.Start(context.TODO())
+
+	// returns context, if signal is created
+	// context.Background() derives a new context (only use to init a context tree)
+	// 'cancel()' will cancel 'ctx' and any of its child context(s) only do at end of function
+	// or use defer cancel()
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("Failed to start app: ", err)
 	}
